8afa0c7c73ca4792b56d797174767969513e0e55
Shammy Raushan <shammyraushan@gmail.com>
2024-05-10 14:06:49 +0530

    Login Page

---
+++ src/Pages/UnderwritingsList.page.js
@@ -0,0 +1,87 @@
+import './Login.css';
+// import LoanImage from "."
+import logo from './pl-imge.jpg';
+
+function UnderWritingList() {
+    return (
+        // <form action="action_page.php" method="post">
+        //     <div class="imgcontainer">
+        //         <img src="img_avatar2.png" alt="Avatar" class="avatar" />
+        //     </div>
+        //     <div class="container">
+        //         <label for="uname">
+        //             <b>Username</b>
+        //         </label>
+        //         <input type="text" placeholder="Enter Username" name="uname" required />
+        //         <label for="psw">
+        //             <b>Password</b>
+        //         </label>
+        //         <input type="password" placeholder="Enter Password" name="psw" required />
+
+        //         <button type="submit">Login</button>
+        //         <label>
+        //             <input type="checkbox" checked="checked" name="remember" /> Remember me
+        //         </label>
+
+        //         <div class="container" style={{ background: "#f1f1f1" }}>
+        //             <button type="button" class="cancelbtn">Cancel</button>
+        //             <span class="psw">Forgot <a href="#">password?</a></span>
+        //         </div>
+        //     </div>
+        // </form>
+
+
+
+
+        <>
+            <div class="list-container">
+                <div class="row-container">
+                    <h4>
+                        NSTP Submission Date
+                    </h4>
+                    <h4>
+                        10/05/2024
+                    </h4>
+
+                </div>
+                <div class="row-container">
+                    <h4>
+                        Product Variant
+                    </h4>
+                    <h4>
+                        PL
+                    </h4>
+                </div>
+                <div class="row-container">
+                    <h4>
+                        Scheme
+                    </h4>
+                </div>
+                <div class="row-container">
+                    <h4>
+                        DLP ID
+                    </h4>
+                </div>
+                <div class="row-container">
+                    <h4>
+                        DLPXXXXXXX
+                    </h4>
+                    <h4>
+                        Raj Khurana
+                    </h4>
+                </div>
+
+                <div class="row-container">
+                    <h4>
+                        Sourcing Channel
+                    </h4>
+                    <h4>
+                        Salaried
+                    </h4>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default UnderWritingList;
