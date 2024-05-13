b86b4fb0a7b993470f6a78982e34cb1fa531a9be
Kishore Purushotham <kishore.p@in.ey.com>
2024-05-13 11:55:09 +0530

    fixed index.js file for react 17 version

--- src/index.js
+++ src/index.js
@@ -1,22 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Login from './Pages/Login.page';
-import UnderWritingList from './Pages/UnderwritingsList.page';
-import UnderwritingDetailsPage from './Pages/UnderwritingDetails.page';
+import React from "react";
+import { render } from "react-dom";
+import "./index.css";
+import UnderwritingDetailsPage from "./Pages/UnderwritingDetails.page";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    {/* <App /> */}
-    {/* <UnderWritingList /> */}
-    <UnderwritingDetailsPage />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = document.getElementById("root");
+render(<UnderwritingDetailsPage />, root);
